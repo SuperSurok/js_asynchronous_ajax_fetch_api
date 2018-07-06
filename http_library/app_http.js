cb91feb46112aea7119bd15b83355892f054c091
Sergey Lebedev <oddyone@mail.ru>
2018-07-06 14:33:23 +0300

    Added requests: POST, PUT, DELETE

--- http_library/app_http.js
+++ http_library/app_http.js
@@ -1,11 +1,63 @@
 const http = new easyHTTP;
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts1',
-    function(err, posts) {
+// Get Request
+// (function() {
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts',
+//     function(err, posts) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(posts);
+//         }
+//     });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/2',
+//     function(err, posts) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(posts);
+//         }
+//     });
+// })();
+
+// Post Request
+
+// Create Data
+const someData = {
+    title: 'Custom Post',
+    body: 'This Custom Post'
+};
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', someData, function(err, post) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5',
+//     someData,
+//     function(err, post) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             console.log(post);
+//         }
+//     });
+
+// Delete Post
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+    function(err, response) {
         if (err) {
             console.log(err)
         } else {
-            console.log(posts);
+            console.log(response);
         }
     });
